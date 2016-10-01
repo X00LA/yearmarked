9793faa32070ac6cf99878205514a1e8580a1ee8
BuildTools <unconfigured@null.spigotmc.org>
2016-10-01 10:44:17 -0400

    renamed Message property files to be prefixed with the word "Yearmarked" this will prevent conflicts with other plugins.

--- src/main/java/com/blocktyper/yearmarked/YearmarkedPlugin.java
+++ src/main/java/com/blocktyper/yearmarked/YearmarkedPlugin.java
@@ -8,14 +8,25 @@ import java.util.ResourceBundle;
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.blocktyper.localehelper.LocaleHelper;
+
 public class YearmarkedPlugin extends JavaPlugin {
 
 	public static Material DEFAULT_MONTH = Material.DIAMOND;
 	public static String KEY_CURRENT_MONTH = "current-month";
+	private String LOCALIZED_KEY_MONTH = "yearmarked.month";
+
+	private LocaleHelper localeHelper = null;
+
+	private String getLocalizedMessage(String key) {
+		return localeHelper == null ? key : localeHelper.getLocalizedMessage(key);
+	}
 
 	public void onEnable() {
 		createConfig();
-
+		File pluginFile = getFile();
+		localeHelper = new LocaleHelper(getLogger(), pluginFile != null ? pluginFile.getParentFile() : null,
+				"YearmarkedMessages");
 		String currentMonth = getConfig().getString(KEY_CURRENT_MONTH);
 		getLogger().info(getLocalizedMessage(LOCALIZED_KEY_MONTH) + ": " + currentMonth);
 
@@ -46,52 +57,4 @@ public class YearmarkedPlugin extends JavaPlugin {
 	}
 	// end config file initialization
 
-	// begin localization
-	private String LOCALIZED_KEY_MONTH = "yearmarked.month";
-	private Locale defaultLocale = Locale.getDefault();
-	private ResourceBundle bundle = null;
-	private boolean bundleLoadFailed = false;
-
-	private String getLocalizedMessage(String key) {
-		
-		if(bundleLoadFailed){
-			return key;
-		}
-		
-		if(bundle == null){
-			try {
-				bundle = ResourceBundle.getBundle("Messages", defaultLocale);
-			} catch (Exception e) {
-				getLogger().warning("Messages bundle did not load successfully from default location.");
-			}
-			if(bundle == null){
-				getLogger().info("Checking for Messages bundle in secondary location (resources/Messages).");
-				try {
-					bundle = ResourceBundle.getBundle("resources/Messages", defaultLocale);
-				} catch (Exception e) {
-					getLogger().warning("Messages bundle did not load successfully from secondary location (resources/Messages).");
-				}
-				
-				if(bundle == null){
-					getLogger().warning("Messages will appear as dot separated key names.  Please remove this plugin from your plugin folder if this behaviour is not desired.");
-					bundleLoadFailed = true;
-					return key;
-				}else{
-					getLogger().info("Messages bundle loaded successfully from secondary location (resources/Messages).");
-				}
-			}else{
-				getLogger().info("Messages bundle loaded successfully from default location.");
-			}
-		}
-		
-		
-		String value = bundle.getString(key);
-		try {
-			value = key != null ? (value != null && !value.trim().isEmpty() ? value : key) : "null key";
-		} catch (Exception e) {
-			value = "error value";
-		}
-		return value;
-	}
-	// end localization
 }
