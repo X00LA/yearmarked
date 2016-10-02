63fc900ecd2546fc5109405da5ab47fe8415b08f
BuildTools <unconfigured@null.spigotmc.org>
2016-10-01 20:02:38 -0400

    now works with LoacaleHelper to load the locale and now loads the bundle internally.

--- src/main/java/com/blocktyper/yearmarked/YearmarkedPlugin.java
+++ src/main/java/com/blocktyper/yearmarked/YearmarkedPlugin.java
@@ -16,17 +16,10 @@ public class YearmarkedPlugin extends JavaPlugin {
 	public static String KEY_CURRENT_MONTH = "current-month";
 	private String LOCALIZED_KEY_MONTH = "yearmarked.month";
 
-	private LocaleHelper localeHelper = null;
-
-	private String getLocalizedMessage(String key) {
-		return localeHelper == null ? key : localeHelper.getLocalizedMessage(key);
-	}
-
 	public void onEnable() {
 		createConfig();
-		File pluginFile = getFile();
-		localeHelper = new LocaleHelper(getLogger(), pluginFile != null ? pluginFile.getParentFile() : null,
-				"YearmarkedMessages");
+		resourceName = "com.blocktyper.yearmarked.resources.YearmarkedMessages";
+		locale = new LocaleHelper(getLogger(), getFile() != null ? getFile().getParentFile() : null).getLocale();
 		String currentMonth = getConfig().getString(KEY_CURRENT_MONTH);
 		getLogger().info(getLocalizedMessage(LOCALIZED_KEY_MONTH) + ": " + currentMonth);
 
@@ -56,5 +49,53 @@ public class YearmarkedPlugin extends JavaPlugin {
 
 	}
 	// end config file initialization
+
+	private Locale locale = null;
+	private ResourceBundle bundle = null;
+	private boolean bundleLoadFailed = false;
+
+	private String resourceName;
+
+	public String getLocalizedMessage(String key) {
+
+		String value = key;
+		try {
+			if (bundle == null) {
+
+				if (locale == null) {
+					getLogger().info("Using default locale.");
+					locale = Locale.getDefault();
+				}
+
+				try {
+					bundle = ResourceBundle.getBundle(resourceName, locale);
+				} catch (Exception e) {
+					getLogger().warning(resourceName + " bundle did not load successfully.");
+				}
+
+				if (bundle == null) {
+					getLogger().warning(
+							"Messages will appear as dot separated key names.  Please remove this plugin from your plugin folder if this behaviour is not desired.");
+					bundleLoadFailed = true;
+					return key;
+				} else {
+					getLogger().info(resourceName + " bundle loaded successfully.");
+				}
+			}
+
+			if (bundleLoadFailed) {
+				return key;
+			}
+
+			value = bundle.getString(key);
+
+			value = key != null ? (value != null && !value.trim().isEmpty() ? value : key) : "null key";
+		} catch (Exception e) {
+			getLogger().warning("Unexpected error getting localized string for key(" + key + "). Message: " + e.getMessage());
+		}
+		return value;
+	}
+
+	// end localization
 
 }
